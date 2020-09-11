d9d7e3f47aedf977ec903781e5509a93d804f308
Max <max@digitalscarcity.io>
2020-09-11 10:37:26 -0400

    initial commit

---
+++ include/test.hpp
@@ -0,0 +1,11 @@
+#include <eosio/eosio.hpp>
+using namespace eosio;
+
+CONTRACT test : public contract {
+   public:
+      using contract::contract;
+
+      ACTION hi( name nm );
+      ACTION bye( name nm );
+
+};
---
+++ src/test.cpp
@@ -0,0 +1,10 @@
+#include <test.hpp>
+ACTION test::hi( name nm ) {
+   /* fill in action body */
+   print_f("hi : %\n",nm);
+}
+
+ACTION test::bye( name nm ) {
+   /* fill in action body */
+   print_f("bye : %\n",nm);
+}
